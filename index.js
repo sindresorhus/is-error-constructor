0016a5435a14a6628d1f35a556ea66dd641f0ce2
Christian Fei <crifei93@gmail.com>
2016-10-23 20:43:16 +0200

    use error constructor
    see
    https://github.com/sindresorhus/is-error-constructor/commit/8bd2e50ad05c32399f03eb38a1ac10008fa350cc#commitcomment-19532070

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 'use strict';
 module.exports = ctor => {
-	let proto = ctor.prototype;
-
-	while (proto) {
-		if (proto === Error.prototype) {
-			return true;
-		}
-
-		proto = Object.getPrototypeOf(proto);
-	}
-
-	return false;
+	return ctor === Error || ctor.prototype instanceof Error;
 };
